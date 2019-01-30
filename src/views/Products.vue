7b5ffc3de96c113922be172a02e8739c0453f0cd
benada002 <45796304+benada002@users.noreply.github.com>
2019-01-30 13:11:51 +0100

    about

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
     <div class="container">
       <div class="card-columns">
         <div class="card" v-for="(post, i) in posts">
