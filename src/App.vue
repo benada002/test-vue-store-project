f865017b8c17d8573300a1dfd478433fd157a3ab
benada002 <45796304+benada002@users.noreply.github.com>
2020-02-27 20:10:54 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
             <router-link to="/" class="nav-link">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/about" class="nav-link">Products</router-link>
+            <router-link to="/products" class="nav-link">Products</router-link>
           </li>
         </ul>
       </div>
